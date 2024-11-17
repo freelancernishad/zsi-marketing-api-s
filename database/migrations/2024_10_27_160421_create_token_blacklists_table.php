d99f3b02ae8da27f3308bd2097df12d2b9bbebe9
Nishad Hossain <freelancernishad123@gmail.com>
2024-11-17 08:19:25 +0600

    update token issue

--- database/migrations/2024_10_27_160421_create_token_blacklists_table.php
+++ database/migrations/2024_10_27_160421_create_token_blacklists_table.php
@@ -9,8 +9,8 @@ class CreateTokenBlacklistsTable extends Migration
     {
         Schema::create('token_blacklists', function (Blueprint $table) {
             $table->id();
-            $table->longText('token');
-            $table->unique('token', 'token_blacklists_token_unique', '255');  // Add length for the index
+            $table->string('token', 65535);  // Change to VARCHAR(65535) if appropriate
+            $table->unique('token');         // Create a unique index
             $table->timestamps();
         });
     }
