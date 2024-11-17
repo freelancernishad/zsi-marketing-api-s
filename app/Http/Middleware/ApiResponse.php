5f4dc6dd53b4a965da7104ab954476ca0cf71fc9
Nishad Hossain <freelancernishad123@gmail.com>
2024-11-17 13:33:52 +0600

    update

--- app/Http/Middleware/ApiResponse.php
+++ app/Http/Middleware/ApiResponse.php
@@ -20,7 +20,7 @@ class ApiResponse
 
         // Skip ApiResponse middleware for the /files/{path} route
         if ($request->is('files/*')) {
-           
+
             return $next($request);
         }
 
@@ -34,31 +34,34 @@ class ApiResponse
 
             // Initialize the formatted response structure
             $formattedResponse = [
-                'encoded' => [
+                // 'encoded' => [
                     'data' => $responseData,
                     'isError' => false,
                     'error' => null,
                     'status_code' => $response->status(),
-                ],
-                'jrn' => microtime(true) * 10000, // Unique journal number
+                // ],
+                // 'jrn' => microtime(true) * 10000,
             ];
 
             // Check if the response status indicates an error (>=400)
             if ($response->status() >= 400) {
-                $formattedResponse['encoded']['isError'] = true;
+                // $formattedResponse['encoded']['isError'] = true;
+                $formattedResponse['isError'] = true;
 
                 // Extract the error message from response data or use a default error message
                 $errorMessage = $responseData['error'] ?? 'An error occurred';
 
                 // Set the error details in the response structure
-                $formattedResponse['encoded']['error'] = [
+                // $formattedResponse['encoded']['error'] = [
+                $formattedResponse['error'] = [
                     'code' => $response->status(),
                     'message' => Response::$statusTexts[$response->status()] ?? 'Unknown error',
                     'errMsg' => $errorMessage,
                 ];
 
                 // Adjust status code if necessary
-                $formattedResponse['encoded']['status_code'] = $response->status();
+                // $formattedResponse['encoded']['status_code'] = $response->status();
+                $formattedResponse['status_code'] = $response->status();
             }
 
             // Return a 200 status code with the formatted response for consistency
